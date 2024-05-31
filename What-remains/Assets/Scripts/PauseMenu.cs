8b9d8ff71a59a88a50f034888f9b25816cb54159
Dext3r58 <dexter22zasm@gmail.com>
2024-05-31 21:46:25 +0300

    Pause Menu
    ADDED PAUSE MENU HIT ESCAPE

---
+++ What-remains/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    public static bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else 
+            {
+                PausedGame();
+            }
+        }
+    }
+
+    public void PausedGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
